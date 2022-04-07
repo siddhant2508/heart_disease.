dd5d915d35570657d983ccfb7af7cdd211f6f295
siddhant2508 <69863184+siddhant2508@users.noreply.github.com>
2022-04-07 22:20:06 +0530

    Update heart_disease.py

--- heart_disease.py
+++ heart_disease.py
@@ -191,7 +191,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB 
 from sklearn.ensemble import RandomForestClassifier
-from mlxtend import StackingCVClassifier
+from mlxtend.classifier import StackingCVClassifier
 scv=StackingCVClassifier(classifiers=[xgb,Knn_clf,RF_clf2],meta_classifier= Knn_clf)
 scv.fit(X_train,Y_train)
 scv_score=scv.score(X_test,Y_test)
