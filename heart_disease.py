e93d25e23f0b06181ad19d3e21da609c43be169c
siddhant2508 <69863184+siddhant2508@users.noreply.github.com>
2022-04-07 11:16:17 +0530

    Create heart_disease.py

---
+++ heart_disease.py
@@ -0,0 +1,118 @@
+from gettext import install
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+df=pd.read_csv("heart.csv")
+df.head()
+
+df.info()
+df.isna().sum()
+
+df.sex.value_counts()
+df.sex[df.target==1].value_counts()
+
+df.sex[df.target==1].value_counts().plot(kind='bar',figsize=(10,6),color=['green','blue'])
+plt.title("Count of the number of males and females with heart disease")
+plt.xticks(rotation=0)
+
+pd.crosstab(df.target,df.sex)
+
+pd.crosstab(df.target,df.sex).plot(kind='bar',figsize=(10,6),color=["lightblue","pink"])
+plt.title("Frequency of Heart Disease vs Sex")
+plt.xlabel("0= Heart Disease, 1= No disease")
+plt.ylabel("Number of people with heart disease")
+plt.legend(["Female","Male"])
+plt.xticks(rotation=0);
+
+df.corr()
+cor_mat=df.corr()
+fig,ax=plt.subplots(figsize=(15,10))
+sns.heatmap(cor_mat,annot=True,linewidths=0.5,fmt=".3f")
+
+from sklearn.preprocessing import MinMaxScaler
+scal=MinMaxScaler()
+feat=['age', 	'sex', 	'cp', 'trestbps', 'chol', 	'fbs', 	'restecg', 	'thalach' ,	'exang', 	'oldpeak' ,	'slope', 	'ca', 'thal']
+df[feat] = scal.fit_transform(df[feat])
+df.head()
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+features= ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+#df[features] = scaler.fit_transform(df[features])
+df.head()
+
+X=df.drop("target",axis=1).values
+Y=df.target.values
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=0,test_size=0.2)
+
+
+from sklearn.metrics import accuracy_score,recall_score,f1_score,precision_score,roc_auc_score,confusion_matrix
+
+def evaluation(Y_test,Y_pred):
+  acc=accuracy_score(Y_test,Y_pred)
+  rcl=recall_score(Y_test,Y_pred)
+  f1=f1_score(Y_test,Y_pred)
+ 
+
+  metric_dict={'accuracy': round(acc,3),
+               'recall': round(rcl,3),
+               'F1 score': round(f1,3),
+               
+              }
+
+  return print(metric_dict)
+
+#evaluation(Y_test,SVC_Y_pred)
+
+np.random.seed(42)
+from sklearn.neighbors import KNeighborsClassifier
+Knn_clf=  KNeighborsClassifier()
+Knn_clf.fit(X_train,Y_train)
+Knn_Y_pred=Knn_clf.predict(X_test)
+Knn_score=Knn_clf.score(X_test,Y_test)
+#print(Knn_score)
+evaluation(Y_test,Knn_Y_pred)
+
+
+np.random.seed(42)
+from sklearn.linear_model import LogisticRegression
+LR_clf=LogisticRegression()
+LR_clf.fit(X_train,Y_train)
+LR_Y_pred=LR_clf.predict(X_test)
+LR_score=LR_clf.score(X_test,Y_test)
+#print(LR_score)
+evaluation(Y_test,LR_Y_pred)
+np.random.seed(42)
+from sklearn.ensemble import RandomForestClassifier
+RF_clf=RandomForestClassifier(n_estimators=450)
+RF_clf.fit(X_train,Y_train)
+RF_score=RF_clf.score(X_test,Y_test)
+RF_Y_pred=RF_clf.predict(X_test)
+#print(RF_score)
+evaluation(Y_test,RF_Y_pred)
+
+np.random.seed(42)
+from sklearn.svm import SVC
+SVC_clf=SVC()
+SVC_clf.fit(X_train,Y_train)
+SVC_score=SVC_clf.score(X_test,Y_test)
+SVC_Y_pred=SVC_clf.predict(X_test)
+#print(SVC_score)
+evaluation(Y_test,SVC_Y_pred)
+
+from xgboost import XGBClassifier
+XGB_clf=XGBClassifier()
+XGB_clf.fit(X_train,Y_train)
+XGB_score=XGB_clf.score(X_test,Y_test)
+XGB_Y_pred=XGB_clf.predict(X_test)
+#print(SVC_score)
+evaluation(Y_test,XGB_Y_pred)
