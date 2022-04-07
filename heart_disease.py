528f17f14d6869b8fb73c23c107d01456cde31f2
siddhant2508 <69863184+siddhant2508@users.noreply.github.com>
2022-04-07 11:33:24 +0530

    Update heart_disease.py

--- heart_disease.py
+++ heart_disease.py
@@ -1,10 +1,8 @@
 from gettext import install
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
@@ -40,7 +38,7 @@ sns.heatmap(cor_mat,annot=True,linewidths=0.5,fmt=".3f")
 
 from sklearn.preprocessing import MinMaxScaler
 scal=MinMaxScaler()
-feat=['age', 	'sex', 	'cp', 'trestbps', 'chol', 	'fbs', 	'restecg', 	'thalach' ,	'exang', 	'oldpeak' ,	'slope', 	'ca', 'thal']
+feat=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach' , 'exang', 'oldpeak' , 'slope', 'ca', 'thal']
 df[feat] = scal.fit_transform(df[feat])
 df.head()
 
@@ -118,3 +116,291 @@ XGB_score=XGB_clf.score(X_test,Y_test)
 XGB_Y_pred=XGB_clf.predict(X_test)
 #print(SVC_score)
 evaluation(Y_test,XGB_Y_pred)
+
+model_comp = pd.DataFrame({'Model': ['Logistic Regression','Random Forest',
+                    'K-Nearest Neighbour','Support Vector Machine',"XGBoost"], 'Accuracy': [LR_score*100,
+                    RF_score*100,Knn_score*100,SVC_score*100,XGB_score*100]})
+model_comp
+
+
+
+neighbors = range(1, 21) # 1 to 20
+
+# Setup algorithm
+knn = KNeighborsClassifier()
+
+# Loop through different neighbors values
+for i in neighbors:
+    knn.set_params(n_neighbors = i) # set neighbors value
+   
+    # Fit the algorithm
+    print(f"Accuracy with {i} no. of neighbors: {knn.fit(X_train, Y_train).score(X_test,Y_test)}%")
+
+np.random.seed(42)
+from sklearn.neighbors import KNeighborsClassifier
+Knn_clf=  KNeighborsClassifier(n_neighbors=7)
+Knn_clf.fit(X_train,Y_train)
+Knn_Y_pred=Knn_clf.predict(X_test)
+Knn_score=Knn_clf.score(X_test,Y_test)
+evaluation(Y_test,Knn_Y_pred)
+
+from sklearn.ensemble import RandomForestClassifier
+np.random.seed(42)
+for i in range(1,40,1):
+  print(f"With {i*10} estimators:")
+  clf2=RandomForestClassifier(n_estimators=i*10,max_depth=i,random_state=i).fit(X_train,Y_train)
+  print(f"Accuracy: {clf2.score(X_test,Y_test)*100:2f}%")
+
+  from sklearn.ensemble import RandomForestClassifier
+RF_clf2=RandomForestClassifier(n_estimators=30,max_depth=3,random_state=3)
+RF_clf2.fit(X_train,Y_train)
+RF2_acc_score=RF_clf2.score(X_test,Y_test)
+RF2_Y_pred=RF_clf2.predict(X_test)
+#print(RF2_acc_score)
+evaluation(Y_test,RF2_Y_pred)
+
+xgb = XGBClassifier(learning_rate=0.01, n_estimators=25, max_depth=15,gamma=0.6, subsample=0.52,colsample_bytree=0.6,seed=27,
+                    reg_lambda=2, booster='dart', colsample_bylevel=0.6, colsample_bynode=0.5)
+
+xgb.fit(X_train,Y_train)
+xgb_score=XGB_clf.score(X_test,Y_test)
+xgb_Y_pred=XGB_clf.predict(X_test)
+#print(SVC_score)
+evaluation(Y_test,xgb_Y_pred)
+
+from sklearn.model_selection import GridSearchCV
+ 
+# defining parameter range
+param_grid = {'C': [0.1, 1,2, 10, 100, 1000],  
+              'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+              'kernel': ['rbf','linear']}  
+ 
+gs_clf = GridSearchCV(SVC(), param_grid,cv=5, refit = True, verbose = 3)
+ 
+# fitting the model for grid search
+gs_clf.fit(X_train, Y_train)
+
+print(gs_clf.best_params_)
+
+print(f"Accuracy score:{gs_clf.score(X_test,Y_test)}%")
+
+
+knn_grid={'n_neighbors': np.arange(1,30,1),
+          'leaf_size': np.arange(1,50,1)}
+
+gs_knn=GridSearchCV(KNeighborsClassifier(),param_grid=knn_grid,cv=5,verbose=True)
+
+gs_knn.fit(X_train, Y_train)
+
+gs_knn.best_params_
+
+print(f"Accuracy score:{gs_knn.score(X_test,Y_test)*100}%")
+
+model_comp = pd.DataFrame({'Model': ['Logistic Regression','Random Forest',
+                    'K-Nearest Neighbour','Support Vector Machine','Extreme Gradient Boost'], 'Accuracy': [LR_score*100,
+                    RF2_acc_score*100,Knn_score*100,SVC_score*100, XGB_score*100]})
+model_comp
+
+from mlxtend.classifier import StackingCVClassifier
+scv=StackingCVClassifier(classifiers=[xgb,Knn_clf,RF_clf2],meta_classifier= Knn_clf)
+scv.fit(X_train,Y_train)
+scv_score=scv.score(X_test,Y_test)
+scv_Y_pred=scv.predict(X_test)
+#print(SVC_score)
+evaluation(Y_test,scv_Y_pred)
+
+model_comp = pd.DataFrame({'Model': ['Logistic Regression','Random Forest',
+                    'K-Nearest Neighbour','Support Vector Machine','Extreme Gradient Boost', 'StackingCV Classifier'], 'Accuracy': [LR_score*100,
+                    RF2_acc_score*100,Knn_score*100,SVC_score*100, XGB_score*100, scv_score*100]})
+model_comp
+
+print(" Best evaluation parameters achieved with KNN:")
+evaluation(Y_test,scv_Y_pred)
+
+final_metrics={'Accuracy': scv.score(X_test,Y_test),
+                   'Precision': precision_score(Y_test,scv_Y_pred),
+                   'Recall': recall_score(Y_test,scv_Y_pred),
+                   'F1': f1_score(Y_test,scv_Y_pred),
+                   'AUC': roc_auc_score(Y_test,scv_Y_pred)}
+
+metrics=pd.DataFrame(final_metrics,index=[0])
+
+metrics.T.plot.bar(title='Final metric evaluation',legend=False);
+
+from sklearn.metrics import confusion_matrix
+
+fig,ax=plt.subplots()
+ax=sns.heatmap(confusion_matrix(Y_test,Knn_Y_pred),annot=True,cbar=True);
+
+user_input=input("Enter the values one by one")
+user_input=user_input.split(",")
+
+
+for i in range(len(user_input)):
+    # convert each item to int type
+    user_input[i] = float(user_input[i])
+
+user_input=np.array(user_input)
+user_input=user_input.reshape(1,-1)
+user_input=scal.transform(user_input)
+scv_Y_pred=scv.predict(user_input)
+if(scv_Y_pred[0]==0):
+  print("Warning! You have chances of getting a heart disease!")
+else:
+  print("You are healthy and are less likely to get a heart disease!")
+
+
+import pickle as pkl
+pkl.dump(Knn_clf,open("final_model.p","wb"))
+import sklearn
+sklearn_version = sklearn.__version__
+print(sklearn_version)
+
+pip  install  streamlit
+pip  install  pyngrok===4.1.1
+from pyngrok import ngrok
+
+
+%%writefile healthy-heart-app.py
+import streamlit as st
+import base64
+import sklearn
+import numpy as np
+import pickle as pkl
+from sklearn.preprocessing import MinMaxScaler
+scal=MinMaxScaler()
+#Load the saved model
+model=pkl.load(open("final_model.p","rb"))
+
+
+
+
+
+st.set_page_config(page_title="Healthy Heart App",page_icon="⚕️",layout="centered",initial_sidebar_state="expanded")
+
+
+
+def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal ):  
+ 
+   
+    # Pre-processing user input  
+    if sex=="male":
+        sex=1
+    else: sex=0
+   
+   
+    if cp=="Typical angina":
+        cp=0
+    elif cp=="Atypical angina":
+        cp=1
+    elif cp=="Non-anginal pain":
+        cp=2
+    elif cp=="Asymptomatic":
+        cp=2
+   
+    if exang=="Yes":
+        exang=1
+    elif exang=="No":
+        exang=0
+ 
+    if fbs=="Yes":
+        fbs=1
+    elif fbs=="No":
+        fbs=0
+ 
+    if slope=="Upsloping: better heart rate with excercise(uncommon)":
+        slope=0
+    elif slope=="Flatsloping: minimal change(typical healthy heart)":
+          slope=1
+    elif slope=="Downsloping: signs of unhealthy heart":
+        slope=2  
+ 
+    if thal=="fixed defect: used to be defect but ok now":
+        thal=6
+    elif thal=="reversable defect: no proper blood movement when excercising":
+        thal=7
+    elif thal=="normal":
+        thal=2.31
+
+    if restecg=="Nothing to note":
+        restecg=0
+    elif restecg=="ST-T Wave abnormality":
+        restecg=1
+    elif restecg=="Possible or definite left ventricular hypertrophy":
+        restecg=2
+
+
+    user_input=[age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal]
+    user_input=np.array(user_input)
+    user_input=user_input.reshape(1,-1)
+    user_input=scal.fit_transform(user_input)
+    prediction = model.predict(user_input)
+
+    return prediction
+
+   
+
+       
+    # front end elements of the web page
+html_temp = """
+    <div style ="background-color:pink;padding:13px">
+    <h1 style ="color:black;text-align:center;">Healthy Heart App</h1>
+    </div>
+    """
+     
+# display the front end aspect
+st.markdown(html_temp, unsafe_allow_html = True)
+st.subheader('by Amlan Mohanty ')
+     
+# following lines create boxes in which user can enter data required to make prediction
+age=st.selectbox ("Age",range(1,121,1))
+sex = st.radio("Select Gender: ", ('male', 'female'))
+cp = st.selectbox('Chest Pain Type',("Typical angina","Atypical angina","Non-anginal pain","Asymptomatic"))
+trestbps=st.selectbox('Resting Blood Sugar',range(1,500,1))
+restecg=st.selectbox('Resting Electrocardiographic Results',("Nothing to note","ST-T Wave abnormality","Possible or definite left ventricular hypertrophy"))
+chol=st.selectbox('Serum Cholestoral in mg/dl',range(1,1000,1))
+fbs=st.radio("Fasting Blood Sugar higher than 120 mg/dl", ['Yes','No'])
+thalach=st.selectbox('Maximum Heart Rate Achieved',range(1,300,1))
+exang=st.selectbox('Exercise Induced Angina',["Yes","No"])
+oldpeak=st.number_input('Oldpeak')
+slope = st.selectbox('Heart Rate Slope',("Upsloping: better heart rate with excercise(uncommon)","Flatsloping: minimal change(typical healthy heart)","Downsloping: signs of unhealthy heart"))
+ca=st.selectbox('Number of Major Vessels Colored by Flourosopy',range(0,5,1))
+thal=st.selectbox('Thalium Stress Result',range(1,8,1))
+
+
+
+#user_input=preprocess(sex,cp,exang, fbs, slope, thal )
+pred=preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,ca,thal)
+
+
+
+
+if st.button("Predict"):    
+  if pred[0] == 0:
+    st.error('Warning! You have high risk of getting a heart attack!')
+   
+  else:
+    st.success('You have lower risk of getting a heart disease!')
+   
+   
+
+
+
+st.sidebar.subheader("About App")
+
+st.sidebar.info("This web app is helps you to find out whether you are at a risk of developing a heart disease.")
+st.sidebar.info("Enter the required fields and click on the 'Predict' button to check whether you have a healthy heart")
+st.sidebar.info("Don't forget to rate this app")
+
+
+
+feedback = st.sidebar.slider('How much would you rate this app?',min_value=0,max_value=5,step=1)
+
+if feedback:
+  st.header("Thank you for rating the app!")
+  st.info("Caution: This is just a prediction and not doctoral advice. Kindly see a doctor if you feel the symptoms persist.")
+
+
+!nohup streamlit run healthy-heart-app.py &
+url = ngrok.connect(port='8501')
+url
