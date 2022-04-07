c2b9394a290b2755d3e9d5d6ac6cb5786f956796
siddhant2508 <69863184+siddhant2508@users.noreply.github.com>
2022-04-08 00:18:32 +0530

    Update heart_disease.py

--- heart_disease.py
+++ heart_disease.py
@@ -574,10 +574,10 @@ def preprocess(age,sex,cp,trestbps,restecg,chol,fbs,thalach,exang,oldpeak,slope,
        
     # front end elements of the web page
    
-html_temp  
+!!!html_temp  
 <div style ="background-color:pink;padding:13px">
     <h1 style ="color:black;text-align:center;">Healthy Heart App</h1>
-    </div>
+    </div>!!!
     
      
 # display the front end aspect
